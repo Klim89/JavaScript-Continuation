471503bb71d41e4381447f0c4d71f2082060377f
Dmitriy <klimov.dima@mail.ru>
2023-11-27 21:08:20 +0300

    HW_5

---
+++ Практика/Homework/HW_5/data.js
@@ -0,0 +1,25 @@
+const dataInfo = `
+[
+    {
+        "albumId": 2,
+        "id": 66,
+        "title": "provident rerum voluptatem illo asperiores qui maiores",
+        "url": "https://via.placeholder.com/600/ee0a7e",
+        "thumbnailUrl": "https://via.placeholder.com/150/ee0a7e"
+    },
+    {
+        "albumId": 2,
+        "id": 67,
+        "title": "veritatis labore ipsum unde aut quam dolores",
+        "url": "https://via.placeholder.com/600/1279e9",
+        "thumbnailUrl": "https://via.placeholder.com/150/1279e9"
+    },
+    {
+        "albumId": 2,
+        "id": 68,
+        "title": "architecto aut quod qui ullam vitae expedita delectus",
+        "url": "https://via.placeholder.com/600/e9603b",
+        "thumbnailUrl": "https://via.placeholder.com/150/e9603b"
+    }
+]
+`
