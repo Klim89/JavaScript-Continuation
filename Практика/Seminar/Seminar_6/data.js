8b7f3cbbee6e6333f61bca0b40ff4cf6a4addab3
Dmitriy <klimov.dima@mail.ru>
2023-12-04 00:18:55 +0300

    Семинар. Шаблонизация

--- Практика/Seminar/Seminar_6/data.js
+++ Практика/Seminar/Seminar_6/data.js
@@ -1,20 +1,18 @@
-let dataProducts = `
-[
+let dataProducts = `[
     {
-        "img": "./Rectangle 15 copy (1).jpg",
-        "title": "MANGO PEOPLE T-SHIRT",
-        "price": $300,
-        "color": "Red",
-        "size": "XL",
-        "quantity": "2"
+    "img": "./Rectangle 15 copy (1).jpg",
+    "title": "MANGO PEOPLE T-SHIRT",
+    "price": "$300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2"
     },
-    {
-        "img": "./Rectangle 15 copy.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "price": $52,
-        "color": "Black",
-        "size": "XL",
-        "quantity": "1"
-    },
-]`;
- 
+        {
+    "img": "./Rectangle 15 copy.jpg",
+    "title": "ELLERY XMO CAPSULE",
+    "price": "52",
+    "color": "Black",
+    "size": "XL",
+    "quantity": "1"
+    }
+    ]`;
